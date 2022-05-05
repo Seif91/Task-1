fe72b764dba0e0fecc90e35c0b0b608ff6910d9c
Seif91 <104800104+Seif91@users.noreply.github.com>
2022-05-05 19:17:07 +0200

    main code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, redirect, flash
-from models import Schema
+from models import Schema #funktion aufrufen
 
 
 app = Flask(__name__) #__main__
@@ -9,20 +9,20 @@ def hellopexonian():
     return "Hello Pexonian, trage hier deine Zertifizierungen ein! "
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"]) #route for uploading file
 def index():
     if request.method == "GET":
         return render_template('index.html')
 
     if not 'file' in request.files:
-        flash('No file part in request')
+        flash('No file part in request')  #No file part in request
         return redirect(request.url)
 
-    files = request.files.getlist('file')
+    files = request.files.getlist('file')  #list for uploading multiple files
 
     for file in files:
         if file.filename == '':
-            flash('No file uploaded')
+            flash('No file uploaded')  #No file uploaded'
             return redirect(request.url)
         else:
             flash('File type not supported')
@@ -33,6 +33,3 @@ def index():
 if __name__ == "__main__":
     Schema()
     app.run(debug = True)
-
-
-
